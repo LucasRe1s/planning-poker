9361090e09dc3f1a0c649c8cbd7b68bd20364d57
Lucas Reis <108764828+LucasRe1s@users.noreply.github.com>
2023-08-19 12:57:11 -0300

    docs: Update Readme.md

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const gamesRouter = require('./routes/games-router');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(gamesRouter);
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('Rodando na porta', port);
+});
+
+module.exports = app;
