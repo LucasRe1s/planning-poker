48b3d57bacfb65b9874e1a27c4dc132f206d3ddf
Lucas Reis <108764828+LucasRe1s@users.noreply.github.com>
2023-09-04 18:34:14 -0300

    feat: create get game by id

--- src/controllers/games-controller.js
+++ src/controllers/games-controller.js
@@ -1,42 +1,80 @@
 const gamesService = require('../services/games-service')
-const playersService = require('../services/create-players')
+const playersService = require('../services/create-players');
+const database = require('../data/database');
+
+let gamesList = [];
 
 function createGame(req, res) {
-    try {
-        const name = req.body.name;
-        const gameCreated = gamesService.createGame({ name })
-        res.status(201).json(gameCreated);
-    } catch (error) {
-        res.status(500).json({ messageError: error.message });
-    }
+  try {
+    const name = req.body.name;
+    const gameCreated = gamesService.createGame({ name })
+    res.status(201).json(gameCreated);
+  } catch (error) {
+    res.status(500).json({ messageError: error.message });
+  }
 }
 
+
+
 function getGames(req, res) {
-    const games = gamesService.getGames()
-    res.status(200).json(games)
+  try {
+    gamesList = gamesService.getGames()
+    res.status(200).json(gamesList)
+  } catch (error) {
+    res.status(500).json({ messageError: error.message })
+  }
 }
 
 function createPlayers(req, res) {
 
-    try {
-        const idUrl = req.params.id;
-        console.log('id url: ' + idUrl)
+  try {
+    const gameId = req.params.id;
+    const name = req.body.name;
+    const gameCreated = playersService.createPlayers({ name, gameId })
+    console.log("name " + name)
+    console.log('game id: ' + gameId)
+    res.status(201).json(gameCreated);
+    const games = gamesService.createGame(gameId);
 
-        const name = req.body.name;
-        const playerCreated = playersService.createPlayers({ name, idUrl })
-        res.status(201).json(playerCreated);
 
-    } catch (error) {
-        res.status(500).json({ messageError: error.message });
+
+
+
+
+
+
+  } catch (error) {
+    res.status(500).json({ messageError: error.message });
+
+  }
+
+
+
+}
+
+
+
+
+async function getGameById(req, res) {
+
+
+  const games = gamesService.getGames()
+  const gameId = req.params.id;
+  
+  for(let i = 0; i < games.length; i++){
+    if (gameId === games[i].id) {
+      let game = games[i]
+     
+      console.log(game)
+      
+      return res.json({game})
+      
     }
-}
-
-function getId(req, res) {
-
-
+    
+  }
 
 }
 
 
-module.exports = { createGame, getGames, createPlayers, getId };
 
+module.exports = { createGame, getGames, createPlayers, getGameById };
